c0c3f2d738be70da73f520ea47ce9ab937e34484
yashrajvrma <vermayashraj2005@gmail.com>
2024-12-24 15:29:09 +0530

    add image upload to cloudinary

--- frontend/src/components/EditorComponent.tsx
+++ frontend/src/components/EditorComponent.tsx
@@ -1,22 +1,50 @@
 import { Editor } from "novel-lightweight";
-import { useState } from "react";
+import { JSONContent } from "@tiptap/core";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface outputData {
+  data: JSONContent;
+}
 
 export default function App() {
-  const [data, setData] = useState("");
-  console.log(data);
+  const [data, setData] = useState<outputData | undefined>(undefined); // console.log(data);
+
+  useEffect(() => {
+    console.log("data is" + JSON.stringify(data, null, 2));
+  }, [data]);
 
   return (
     <Editor
       className="bg-transparent font-sans text-base"
-      defaultValue={data}
+      defaultValue={""}
       disableLocalStorage={true}
       onUpdate={(editor) => {
-        setData(editor?.storage.markdown.getMarkdown());
+        const jsonData = editor?.getJSON();
+        if (jsonData) {
+          setData({ data: jsonData }); // Update state with the correct structure
+        }
       }}
+      // onUpdate={(editor) => {
+      //   setData(editor?.storage.markdown.getMarkdown());
+      // }}
       handleImageUpload={async (file) => {
-        const uploads = await startUpload([file]);
-        if (uploads && uploads.length > 0) {
-          return uploads[0].url;
+        if (!file) {
+          return "No file selected";
+        }
+        const data = new FormData();
+        data.append("file", file);
+        data.append("upload_preset", "MediumBlog-Images");
+        data.append("cloud_name", "dspu4ehu3");
+
+        const response = await axios.post(
+          "https://api.cloudinary.com/v1_1/dspu4ehu3/image/upload",
+          data
+        );
+        const imageUrl = response.data.url;
+
+        if (imageUrl) {
+          return imageUrl;
         }
         return "www.example.com/failed-upload.png";
       }}
