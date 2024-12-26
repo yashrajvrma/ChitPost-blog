c7998eb585049c38fde115801ff7a4b32155ccf1
yashrajvrma <vermayashraj2005@gmail.com>
2024-12-26 12:40:48 +0530

    remove require from tailwind configjs file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,6 @@
+import typography from "@tailwindcss/typography";
+import animate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
@@ -66,6 +69,5 @@ export default {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography"), require("tailwindcss-animate")],
-  // plugins: [require("tailwindcss-animate")],
+  plugins: [typography, animate],
 };
