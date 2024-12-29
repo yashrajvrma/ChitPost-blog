e3f835f4ffed315b06275cb7d89c8e3772566f91
yashrajvrma <vermayashraj2005@gmail.com>
2024-12-29 13:34:04 +0530

    add vercel analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,11 +5,14 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor } from "./app/store";
 import store from "./app/store.ts";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <App />
+      {/* <Analytics /> */}
     </PersistGate>
+    <Analytics />
   </Provider>
 );
