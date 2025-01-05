6a7dc9ea508643fe341adca2e30dd3f330125607
yashrajvrma <vermayashraj2005@gmail.com>
2025-01-06 02:59:25 +0530

    change vite.config.ts to remove console log

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // esbuild: {
-  //   drop: ["console", "debugger"],
-  // },
+  esbuild: {
+    drop: ["console", "debugger"],
+  },
 });
